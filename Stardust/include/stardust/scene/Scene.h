ca121cda46f49bfa6d1063b77079b68d5652786d
Matt Schafer <lucidsigma17@gmail.com>
2020-12-17 22:16:01 +1000

    Add Scene class

---
+++ Stardust/include/stardust/scene/Scene.h
@@ -0,0 +1,52 @@
+#pragma once
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "stardust/utility/interfaces/INoncopyable.h"
+#include "stardust/utility/interfaces/INonmovable.h"
+
+#include <SDL2/SDL.h>
+
+#include "stardust/data/Containers.h"
+#include "stardust/utility/enums/Status.h"
+
+namespace stardust
+{
+	class Scene
+		: private INoncopyable, private INonmovable
+	{
+	private:
+		const String m_name;
+
+	protected:
+		class Application& m_application;
+		// entt::registry& m_entityRegistry;
+
+	public:
+		Scene(class Application& application, const String& name);
+		virtual ~Scene() noexcept = default;
+
+		[[nodiscard]] virtual Status OnLoad() = 0;
+		virtual void OnUnload() noexcept = 0;
+
+		inline virtual void FixedUpdate(const float fixedDeltaTime) { };
+		virtual void ProcessInput() = 0;
+		virtual void Update(const float deltaTime) = 0;
+		inline virtual void LateUpdate(const float deltaTime) { }
+
+		// virtual void Render(const Renderer& renderer) const = 0;
+
+		inline virtual void PollEvents(SDL_Event& event) { }
+
+		// inline entt::registry& GetEntityRegistry() noexcept { return m_entityRegistry; }
+		inline const String& GetName() const noexcept { return m_name; }
+
+	protected:
+		// class Entity CreateEntity();
+
+	private:
+		// Add helper functions to make getting data from the main Application much easier.
+	};
+}
+
+#endif
---
+++ Stardust/src/scene/Scene.cpp
@@ -0,0 +1,10 @@
+#include "stardust/scene/Scene.h"
+
+#include "stardust/application/Application.h"
+
+namespace stardust
+{
+	Scene::Scene(Application& application, const String& name)
+		: m_name(name), m_application(application)//, m_entityRegistry(application.GetEntityRegistry())
+	{ }
+}
