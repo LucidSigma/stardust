f286dca2bbcdcb55ca7c8e4a11d6e8ebdfc1b1b5
Matt Schafer <lucidsigma17@gmail.com>
2020-12-19 22:07:19 +1000

    Implement random functionality from old build

---
+++ Stardust/include/stardust/utility/random/Random.h
@@ -0,0 +1,71 @@
+#pragma once
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include "stardust/utility/interfaces/INoncopyable.h"
+#include "stardust/utility/interfaces/INonmovable.h"
+
+#include <concepts>
+#include <random>
+
+#include "stardust/data/Containers.h"
+#include "stardust/data/Types.h"
+
+namespace stardust
+{
+	class Random
+		: private INoncopyable, private INonmovable
+	{
+	public:
+		enum class BinomialDistributionType
+		{
+			Binomial,
+			NegativeBinomial,
+			Geometric
+		};
+
+	private:
+		inline static std::mt19937 s_randomEngine{ std::random_device()() };
+
+	public:
+		static void ReSeed();
+
+		template <std::floating_point T = f32>
+		[[nodiscard]] static T GenerateFloat(const T min = 0.0f, const T max = 1.0f)
+		{
+			const std::uniform_real_distribution<T> uniformDistribution(min, max);
+
+			return uniformDistribution(s_randomEngine);
+		}
+
+		template <std::integral T = i32>
+		[[nodiscard]] static T GenerateInt(const T min, const T max)
+		{
+			const std::uniform_int_distribution<T> uniformDistribution(min, max);
+
+			return uniformDistribution(s_randomEngine);
+		}
+
+		[[nodiscard]] static bool FlipCoin();
+		[[nodiscard]] static u32 RollDice(const u32 sideCount = 6u);
+
+		[[nodiscard]] static bool BernoulliDistribution(const f32 probability);
+		[[nodiscard]] static u32 BinomialDistribution(const u32 trials, const f32 probability, const BinomialDistributionType distributionType = BinomialDistributionType::Binomial);
+		[[nodiscard]] static u32 PoissonDistribution(const u32 frequency);
+
+		template <std::floating_point T = f32>
+		[[nodiscard]] static T NormalDistribution(const T mean, const T standardDeviation)
+		{
+			std::normal_distribution<T> normalDistribution(mean, standardDeviation);
+
+			return normalDistribution(s_randomEngine);
+		}
+
+		[[nodiscard]] static usize DiscreteDistribution(const Vector<f64>& weights);
+
+		Random() = delete;
+		~Random() noexcept = delete;
+	};
+}
+
+#endif
---
+++ Stardust/src/utility/random/Random.cpp
@@ -0,0 +1,71 @@
+#include "stardust/utility/random/Random.h"
+
+#include <algorithm>
+#include <iterator>
+
+namespace stardust
+{
+	void Random::ReSeed()
+	{
+		s_randomEngine.seed(std::random_device()());
+	}
+
+	[[nodiscard]] bool Random::FlipCoin()
+	{
+		return GenerateInt(0, 1) == 0;
+	}
+
+	[[nodiscard]] u32 Random::RollDice(const u32 sideCount)
+	{
+		return GenerateInt<u32>(1u, sideCount);
+	}
+
+	[[nodiscard]] bool Random::BernoulliDistribution(const f32 probability)
+	{
+		const std::bernoulli_distribution bernoulliDistribution(std::clamp(probability, 0.0f, 1.0f));
+
+		return bernoulliDistribution(s_randomEngine);
+	}
+
+	[[nodiscard]] u32 Random::BinomialDistribution(const u32 trials, const f32 probability, const BinomialDistributionType distributionType)
+	{
+		switch (distributionType)
+		{
+		case BinomialDistributionType::NegativeBinomial:
+		{
+			const std::negative_binomial_distribution negativeNinomialDistribution(trials, std::clamp(probability, 0.0f, 1.0f));
+
+			return negativeNinomialDistribution(s_randomEngine);
+		}
+
+		case BinomialDistributionType::Geometric:
+		{
+			const std::geometric_distribution<u32> geometricDistribution(std::clamp(probability, 0.0f, 1.0f));
+
+			return geometricDistribution(s_randomEngine);
+		}
+
+		case BinomialDistributionType::Binomial:
+		default:
+		{
+			const std::binomial_distribution binomialDistribution(trials, std::clamp(probability, 0.0f, 1.0f));
+
+			return binomialDistribution(s_randomEngine);
+		}
+		}
+	}
+
+	[[nodiscard]] u32 Random::PoissonDistribution(const u32 frequency)
+	{
+		const std::poisson_distribution poissonDistribution(frequency);
+
+		return poissonDistribution(s_randomEngine);
+	}
+
+	[[nodiscard]] usize Random::DiscreteDistribution(const Vector<f64>& weights)
+	{
+		const std::discrete_distribution discreteDistribution(eastl::cbegin(weights), eastl::cend(weights));
+
+		return discreteDistribution(s_randomEngine);
+	}
+}
