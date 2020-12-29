f085578e87f945989d09c5df224bf3dd1cfbb906
Matt Schafer <lucidsigma17@gmail.com>
2020-12-29 18:59:03 +1000

    Create skeleton game controller class

--- Stardust/include/stardust/input/controller/GameController.h
+++ Stardust/include/stardust/input/controller/GameController.h
@@ -2,9 +2,115 @@
 #ifndef STARDUST_GAME_CONTROLLER_H
 #define STARDUST_GAME_CONTROLLER_H
 
+#include "stardust/utility/interfaces/INoncopyable.h"
+
+#include <SDL2/SDL.h>
+
+#include "stardust/data/Containers.h"
+#include "stardust/data/Pointers.h"
+#include "stardust/data/Types.h"
+#include "stardust/input/controller/GameControllerCodes.h"
+
 namespace stardust
 {
+	class GameController
+		: private INoncopyable
 
+	{
+	public:
+		struct ButtonState
+		{
+			struct DPad
+			{
+				bool up;
+				bool down;
+				bool left;
+				bool right;
+			} dPad;
+
+			bool a;
+			bool b;
+			bool x;
+			bool y;
+
+			bool back;
+			bool guide;
+			bool start;
+
+			bool leftStick;
+			bool rightStick;
+
+			bool leftShoulder;
+			bool rightShoulder;
+
+			bool misc;
+			bool touchPad;
+
+			Array<bool, 4u> paddles;
+		};
+
+		struct Axes
+		{
+			struct Thumbstick
+			{
+				i16 x;
+				i16 y;
+			};
+
+			Thumbstick left;
+			Thumbstick right;
+
+			i16 leftTrigger;
+			i16 rightTrigger;
+		};
+
+	private:
+		struct GameControllerDestroyer
+		{
+			void operator ()(SDL_GameController* const gameController) const noexcept;
+		};
+
+		i32 m_id = 0;
+		u32 m_playerIndex = 0u;
+
+		ButtonState m_currentButtons;
+		ButtonState m_previousButtons;
+		Axes m_axes;
+
+		UniquePtr<SDL_GameController, GameControllerDestroyer> m_handle = nullptr;
+
+	public:
+		friend class Input;
+
+		explicit GameController(const int id);
+		GameController(GameController&& other) noexcept;
+		GameController& operator =(GameController&& other) noexcept;
+		~GameController() noexcept = default;
+
+		[[nodiscard]] bool IsButtonDown(const GameControllerButton button) const;
+		[[nodiscard]] bool IsButtonPressed(const GameControllerButton button) const;
+		[[nodiscard]] bool IsButtonUp(const GameControllerButton button) const;
+
+		[[nodiscard]] bool IsAnyButtonDown(const Vector<GameControllerButton>& buttons) const;
+		[[nodiscard]] bool IsAnyButtonPressed(const Vector<GameControllerButton>& buttons) const;
+		[[nodiscard]] bool IsAnyButtonUp(const Vector<GameControllerButton>& buttons) const;
+
+		[[nodiscard]] bool AreAllButtonsDown(const Vector<GameControllerButton>& buttons) const;
+		[[nodiscard]] bool AreAllButtonsPressed(const Vector<GameControllerButton>& buttons) const;
+		[[nodiscard]] bool AreAllButtonsUp(const Vector<GameControllerButton>& buttons) const;
+
+		inline i32 GetID() const noexcept { return m_id; }
+		inline const Axes& GetAxes() const noexcept { return m_axes; }
+
+		inline u32 GetPlayerIndex() const noexcept { return m_playerIndex; }
+		inline void SetPlayerIndex(const u32 playerIndex) noexcept { m_playerIndex = playerIndex; }
+
+		[[nodiscard]] inline SDL_GameController* const GetRawHandle() const noexcept { return m_handle.get(); }
+		[[nodiscard]] SDL_Joystick* const GetRawJoystickHandle() const;
+
+	private:
+		static bool GetButtonState(const GameControllerButton button, const ButtonState& buttonState) noexcept;
+	};
 }
 
 #endif
--- Stardust/include/stardust/input/controller/GameControllerCodes.h
+++ Stardust/include/stardust/input/controller/GameControllerCodes.h
@@ -2,9 +2,47 @@
 #ifndef STARDUST_GAME_CONTROLLER_CODES_H
 #define STARDUST_GAME_CONTROLLER_CODES_H
 
+#include <type_traits>
+
+#include <magic_enum/magic_enum.hpp>
+#include <SDL2/SDL.h>
+
+#include "stardust/data/Types.h"
+
 namespace stardust
 {
+	enum class GameControllerButton
+		: std::underlying_type_t<SDL_GameControllerButton>
+	{
+		DPadUp = SDL_CONTROLLER_BUTTON_DPAD_UP,
+		DPadDown = SDL_CONTROLLER_BUTTON_DPAD_DOWN,
+		DPadLeft = SDL_CONTROLLER_BUTTON_DPAD_LEFT,
+		DPadRight = SDL_CONTROLLER_BUTTON_DPAD_RIGHT,
 
+		A = SDL_CONTROLLER_BUTTON_A,
+		B = SDL_CONTROLLER_BUTTON_B,
+		X = SDL_CONTROLLER_BUTTON_X,
+		Y = SDL_CONTROLLER_BUTTON_Y,
+
+		Back = SDL_CONTROLLER_BUTTON_BACK,
+		Guide = SDL_CONTROLLER_BUTTON_GUIDE,
+		Start = SDL_CONTROLLER_BUTTON_START,
+
+		LeftStick = SDL_CONTROLLER_BUTTON_LEFTSTICK,
+		RightStick = SDL_CONTROLLER_BUTTON_RIGHTSTICK,
+		LeftShoulder = SDL_CONTROLLER_BUTTON_LEFTSHOULDER,
+		RightShoulder = SDL_CONTROLLER_BUTTON_RIGHTSHOULDER,
+
+		Paddle1 = SDL_CONTROLLER_BUTTON_PADDLE1,
+		Paddle2 = SDL_CONTROLLER_BUTTON_PADDLE2,
+		Paddle3 = SDL_CONTROLLER_BUTTON_PADDLE3,
+		Paddle4 = SDL_CONTROLLER_BUTTON_PADDLE4,
+
+		Misc = SDL_CONTROLLER_BUTTON_MISC1,
+		TouchPad = SDL_CONTROLLER_BUTTON_TOUCHPAD,
+	};
+
+	constexpr usize GameControllerButtonCount = magic_enum::enum_count<GameControllerButton>();
 }
 
 #endif
