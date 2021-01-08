137b000e579a145fbe42d2de3126bda7f3ea4c13
Matt Schafer <lucidsigma17@gmail.com>
2021-01-08 12:12:18 +1000

    Tweak tilemap rendering algorithm slightly

--- Stardust/src/tilemap/Tilemap.cpp
+++ Stardust/src/tilemap/Tilemap.cpp
@@ -103,15 +103,10 @@ namespace stardust
 		const f32 scaledCameraHalfHeight = camera.GetHalfSize() / camera.GetAspectRatio() / glm::abs(camera.GetZoom());
 		const f32 zoomFactor = std::max(1.0f, 1.0f / glm::abs(camera.GetZoom()));
 
-		const f32 tilesWidth = 2.0f * (scaledCameraHalfWidth / glm::abs(glm::cos(glm::radians(camera.GetRotation()))) + zoomFactor);
-		const f32 tilesHeight = 2.0f * (scaledCameraHalfHeight / glm::abs(glm::cos(glm::radians(camera.GetRotation()))) + zoomFactor);
+		const f32 tilesWidth = 2.0f * (scaledCameraHalfWidth / glm::abs(glm::cos(glm::radians(camera.GetRotation()))) + zoomFactor + 1.0f);
+		const f32 tilesHeight = 2.0f * (scaledCameraHalfHeight / glm::abs(glm::cos(glm::radians(camera.GetRotation()))) + zoomFactor + 1.0f);
 		const f32 leftmostPossibleTile = ((camera.GetPosition().x - m_position.x) - (tilesWidth / 2.0f)) / m_tileSize.x;
 		const f32 bottommostPossibleTile = ((camera.GetPosition().y - m_position.y) - (tilesHeight / 2.0f)) / m_tileSize.y;
-
-		if (bottommostPossibleTile > 0.0f)
-		{
-			return;
-		}
 
 		components::Transform tileTransform(m_position, 0.0f, NullOpt, m_tileSize);
 
