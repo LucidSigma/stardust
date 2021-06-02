c1e70ec340557306bf72fb749483950798f85947
Matt Schafer <lucidsigma17@gmail.com>
2021-06-02 11:44:10 +1000

    Update PowerShell compress script

--- resources/compress.ps1
+++ resources/compress.ps1
@@ -1,15 +1,13 @@
-$archives = Get-ChildItem -Path .\ -Include *.pak -File -Name
-
-$archives | ForEach-Object {
-	Write-Output "Deleting old $_..."
-	Remove-Item $_
-}
-
 "assets", "locales" | ForEach-Object {
+	if (Test-Path ".\$_.pak" -PathType Leaf) {
+		Write-Output "Deleting old $_..."
+		Remove-Item ".\$_.pak"
+	}
+
 	Write-Output "Compressing $_.pak..."
 	Compress-Archive -Path ".\$_" -DestinationPath ".\$_.zip" -CompressionLevel Optimal
 	Rename-Item -Path ".\$_.zip" -NewName ".\$_.pak"
 }
 
-Write-Output "New archives created successfully."
+Write-Output "New archives created."
 Read-Host
