46e6f37f73909d5daf3b49847879248c3f6ecc4c
Matt Schafer <lucidsigma17@gmail.com>
2021-06-12 10:54:58 +1000

    Implement string function benchmarks

--- benchmarks/string/src/Main.cpp
+++ benchmarks/string/src/Main.cpp
@@ -1,10 +1,94 @@
 #include <benchmark/benchmark.h>
+#include <stardust/Stardust.h>
 
-void BM_Test(benchmark::State& state)
+static void BM_SplitString(benchmark::State& state)
 {
-
+    for (auto execution : state)
+    {
+        const auto result = sd::string::Split("A B C", ' ');
+    }
 }
 
-BENCHMARK(BM_Test);
+BENCHMARK(BM_SplitString);
+
+static void BM_MakeUpper(benchmark::State& state)
+{
+    for (auto execution : state)
+    {
+        const auto result = sd::string::MakeUpper("hello");
+    }
+}
+
+BENCHMARK(BM_MakeUpper);
+
+static void BM_MakeLower(benchmark::State& state)
+{
+    for (auto execution : state)
+    {
+        const auto result = sd::string::MakeLower("HELLO");
+    }
+}
+
+BENCHMARK(BM_MakeLower);
+
+static void BM_RemoveFirstCharacter(benchmark::State& state)
+{
+    for (auto execution : state)
+    {
+        const auto result = sd::string::RemoveFirstCharacter("hello");
+    }
+}
+
+BENCHMARK(BM_RemoveFirstCharacter);
+
+static void BM_RemoveLastCharacter(benchmark::State& state)
+{
+    for (auto execution : state)
+    {
+        const auto result = sd::string::RemoveLastCharacter("hello");
+    }
+}
+
+BENCHMARK(BM_RemoveLastCharacter);
+
+static void BM_RemoveFirstAndLastCharacters(benchmark::State& state)
+{
+    for (auto execution : state)
+    {
+        const auto result = sd::string::RemoveFirstAndLastCharacters("hello");
+    }
+}
+
+BENCHMARK(BM_RemoveFirstAndLastCharacters);
+
+static void BM_LeftTrim(benchmark::State& state)
+{
+    for (auto execution : state)
+    {
+        const auto result = sd::string::LeftTrim("    hello");
+    }
+}
+
+BENCHMARK(BM_LeftTrim);
+
+static void BM_RightTrim(benchmark::State& state)
+{
+    for (auto execution : state)
+    {
+        const auto result = sd::string::RightTrim("hello    ");
+    }
+}
+
+BENCHMARK(BM_RightTrim);
+
+static void BM_Trim(benchmark::State& state)
+{
+    for (auto execution : state)
+    {
+        const auto result = sd::string::Trim("    hello    ");
+    }
+}
+
+BENCHMARK(BM_Trim);
 
 BENCHMARK_MAIN();
